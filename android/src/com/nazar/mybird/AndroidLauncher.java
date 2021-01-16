0830228b28338a2f56ae89e3087727fee0aef041
Nazarii Denha <dengaaa2002@gmail.com>
2021-01-16 12:44:32 +0200

    Initial commit, LibGdx generated files

---
+++ android/src/com/nazar/mybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nazar.mybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nazar.mybird.MyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyBird(), config);
+	}
+}
